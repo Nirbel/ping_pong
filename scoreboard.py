f8d82e35072cbe1545c6c197af32fc9ade222e90
Nirbel99 <nir_229@icloud.com>
2022-08-08 23:35:36 +0300

    pong game

--- scoreboard.py
+++ scoreboard.py
@@ -30,10 +30,6 @@ class ScoreBoard(Turtle):
         self.r_score += 1
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.clear()
-        self.write("GAME OVER!!", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
